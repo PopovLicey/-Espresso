39582fe8bdc680b83a0e84aade0e5561888af0d2
PopovLicey <vp77470@dmail.com>
2020-01-08 17:14:18 +0500

    Firat - 2

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from random import randint
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
-import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from random import randint
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(550, 461)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(210, 350, 75, 61))
-        self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 550, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "PUSH"))
-
-
-class MyWidget(Ui_MainWindow, QMainWindow):
+class MyWidget(QMainWindow):
     def _init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
@@ -51,10 +23,7 @@ class MyWidget(Ui_MainWindow, QMainWindow):
 
     def drawCircle1(self, qp):
         r2 = randint(1, 210)
-        r = randint(1, 255)
-        g = randint(1, 255)
-        b = randint(1, 255)
-        qp.setBrush(QColor(r, g, b))
+        qp.setBrush(QColor('yellow'))
         qp.drawEllipse(275, 210, r2, r2)
 
 
